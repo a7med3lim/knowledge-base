bafaaa2e7f8a9e316e1104af007a79e7c93f3ffd
a7med3lim <a7med3lim@users.noreply.github.com>
2021-11-24 09:14:23 +0400

    upload project

---
+++ js/pagesJs/main.js
@@ -0,0 +1,7 @@
+		// For Side Nave
+        $(function () {
+            $("#sidebarCollapse").on("click", function () {
+                $("#sidebar, #content").toggleClass("active");
+            });
+        });
+        // !!End Side Nave
