a90069407924d4bbe875e6c748e7cf0b20763fd4
a7med3lim <a7med3lim@users.noreply.github.com>
2021-11-24 12:18:53 +0400

    update24-11-2021

---
+++ js/pagesJs/tb_BRL_Requird_Documents.js
@@ -0,0 +1,73 @@
+
+
+// UI Fix  //fix Table Header in modal
+$(document).on('shown.bs.modal', function () {
+	// dt.columns.adjust();
+});
+// UI Fix //fix Table Header in tabs
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+	e.target // newly activated tab
+	e.relatedTarget // previous active tab
+	dt.columns.adjust();
+	dt2.columns.adjust();
+	dt3.columns.adjust();
+	dt4.columns.adjust();
+})
+/******************************************************* */
+// //call Data ConsumerProtection
+let dt = $('#dt-vertical-scroll-BRL-Req-Doc-CreateBusiness').DataTable({
+	initComplete: function () {
+		this.api().columns().every( function () {
+			var column = this;
+			var select = $('<select  class="browser-default custom-select form-control-sm"><option value="" selected>Search</option></select>')
+				.appendTo( $(column.footer()).empty() )
+				.on( 'change', function () {
+					var val = $.fn.dataTable.util.escapeRegex(
+						$(this).val()
+					);
+  
+					column
+						.search( val ? '^'+val+'$' : '', true, false )
+						.draw();
+				} );
+  
+			column.data().unique().sort().each( function ( d, j ) {
+				select.append( '<option value="'+d+'">'+d+'</option>' )
+			} );
+		} );
+	},
+	scrollY: 540,
+	paging: true,
+	filter:true,
+	"pagingType": "simple_numbers",
+	"ajax": {
+		"url": "page_Consumer_protection_data/tb_consumer_protection.txt",
+		"dataSrc": ""
+	},
+	"columns": [
+		{ "data": "number" },
+		{ "data": "position" },
+		{ "data": "notes" }
+	]
+});
+
+/******************************************************* */
+// UI Fix // set layout Table ConsumerProtection
+$("#dt-vertical-scroll-BRL-Req-Doc-CreateBusiness").ready(function () {
+	$("#dt-vertical-scroll-BRL-Req-Doc-CreateBusiness_wrapper").find(".row .col:nth-child(2)").addClass('col-2');
+	$("#dt-vertical-scroll-BRL-Req-Doc-CreateBusiness_wrapper").find(".form-control").css("height", "41px")
+	$("#dt-vertical-scroll-BRL-Req-Doc-CreateBusiness_wrapper").find(".form-control").attr("placeholder", "اكتب هنا كلمات المراد البحث عنها");
+	$("#dt-vertical-scroll-BRL-Req-Doc-CreateBusiness_wrapper").find(".form-control").focus();
+});
+
+
+
+//check if the URL(Browser) contains a given from index page
+//Open Modal modal_tb_consumer_protection From index page
+// if (window.location.href.indexOf("#table_Consumer_Protection") > -1) {
+// 	$("#modal_tb_consumer_protection").modal("show");
+// }
+//Open Modal modal_tb_Outside_consumer_protection From index page
+// if (window.location.href.indexOf("#table_Outside_consumer_protection") > -1) {
+// 	$("#modal_tb__outside_consumer_protection").modal("show");
+// }
