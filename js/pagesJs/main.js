def82ef95bd5a01a518fe47e19cee96f5378ed92
unknown <a7med3lim@gmail.com>
2021-11-30 14:50:12 +0200

    cxzczx
    cxzcxzzxc

--- js/pagesJs/main.js
+++
@@ -1,7 +0,0 @@
-		// For Side Nave
-        $(function () {
-            $("#sidebarCollapse").on("click", function () {
-                $("#sidebar, #content").toggleClass("active");
-            });
-        });
-        // !!End Side Nave
